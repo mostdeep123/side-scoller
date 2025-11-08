a352abca7da87737fcead774c27f626a46705b34
mostdeep <faziletfazilet61@gmail.com>
2025-11-08 11:33:41 +0700

    Fix Coin Sounds

--- Assets/Code/DistanceMeter.cs
+++ Assets/Code/DistanceMeter.cs
@@ -1,57 +1,57 @@
-using UnityEngine;
-using UnityEngine.UI;
-using Cysharp.Threading.Tasks;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using Cysharp.Threading.Tasks;
 
-public class DistanceMeter : MonoBehaviour
-{
-   [Header("Health Bar")]
-    public Image content;
-    public float currentDistance;
-    public float maxCurrentDistance = 10f;
-
-    [Header("Settings")]
-    public float increasePerSecond = 1f;
-
-    bool showDistance;
-
-    void Start()
+    public class DistanceMeter : MonoBehaviour
     {
-        _ = IncreaseDistanceLoop();
-    }
+    [Header("Health Bar")]
+        public Image content;
+        public float currentDistance;
+        public float maxCurrentDistance = 10f;
 
-    private async UniTask IncreaseDistanceLoop()
-    {
-        while (currentDistance < maxCurrentDistance && this != null && gameObject.activeInHierarchy)
+        [Header("Settings")]
+        public float increasePerSecond = 1f;
+
+        bool showDistance;
+
+        void Start()
         {
-            // Add the distance
-            if(GameState.game.state != GameState.gameState.Hit) currentDistance += increasePerSecond * Time.deltaTime;
+            _ = IncreaseDistanceLoop();
+        }
 
-            // Clamp Max
-            currentDistance = Mathf.Min(currentDistance, maxCurrentDistance);
-            if (content != null && GameState.game.state != GameState.gameState.End)
-                content.fillAmount = currentDistance / maxCurrentDistance;
-
-            //set distance reached
-            if(!showDistance && GameState.game.state == GameState.gameState.End)
+        private async UniTask IncreaseDistanceLoop()
+        {
+            while (currentDistance < maxCurrentDistance && this != null && gameObject.activeInHierarchy)
             {
-                showDistance = true;
-                EndPopUp endPopUp = GameState.game.popUpEnd.GetComponent<EndPopUp>();
-                endPopUp.ShowDistanceText(currentDistance);
+                // Add the distance
+                if(GameState.game.state != GameState.gameState.Hit) currentDistance += increasePerSecond * Time.deltaTime;
+
+                // Clamp Max
+                currentDistance = Mathf.Min(currentDistance, maxCurrentDistance);
+                if (content != null && GameState.game.state != GameState.gameState.End)
+                    content.fillAmount = currentDistance / maxCurrentDistance;
+
+                //set distance reached
+                if(!showDistance && GameState.game.state == GameState.gameState.End)
+                {
+                    showDistance = true;
+                    EndPopUp endPopUp = GameState.game.popUpEnd.GetComponent<EndPopUp>();
+                    endPopUp.ShowDistanceText(currentDistance);
+                }
+
+                await UniTask.Yield(PlayerLoopTiming.Update);
             }
 
-            await UniTask.Yield(PlayerLoopTiming.Update);
-        }
+            if (currentDistance >= maxCurrentDistance)
+            {
+                EndPopUp endPopUp = GameState.game.popUpEnd.GetComponent<EndPopUp>();
+                endPopUp.ShowDistanceText(currentDistance);
+                GameState.game.state = GameState.gameState.End;
+                GameState.game.UpdateState();
+            }
 
-        if (currentDistance >= maxCurrentDistance)
-        {
-            EndPopUp endPopUp = GameState.game.popUpEnd.GetComponent<EndPopUp>();
-            endPopUp.ShowDistanceText(currentDistance);
-            GameState.game.state = GameState.gameState.End;
-            GameState.game.UpdateState();
+            // Make Sure Fill is full
+            if (content != null)
+                content.fillAmount = 1f;
         }
-
-        // Make Sure Fill is full
-        if (content != null)
-            content.fillAmount = 1f;
     }
-}
--- Assets/Code/Hit.cs
+++ Assets/Code/Hit.cs
@@ -19,6 +19,12 @@ public class Hit : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.CompareTag("obstacle"))
+        {
+            GameState.game.state = GameState.gameState.Hit;
+            GameState.game.UpdateState();
+        }
+
         if (collision.CompareTag("obstacle") && !isHit)
         {
             _ = FlashRedAsync();
@@ -27,12 +33,12 @@ public class Hit : MonoBehaviour
 
     private async UniTask FlashRedAsync()
     {
-        GameState.game.state = GameState.gameState.Hit;
-        GameState.game.UpdateState();
         isHit = true;
         sr.color = hitColor;
         await UniTask.Delay((int)(hitDuration * 1000));
         sr.color = originalColor;
         isHit = false;
+        GameState.game.state = GameState.gameState.Run;
+        GameState.game.UpdateState();
     }
 }
