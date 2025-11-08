d312c95392d717bf39d397cce40179af47cb4dc1
mostdeep <faziletfazilet61@gmail.com>
2025-11-08 11:04:19 +0700

    Glide Fix and BGM

---
+++ Assets/Code/Hit.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class Hit : MonoBehaviour
+{
+    [Header("Hit Effect Settings")]
+    public Color hitColor = Color.red;
+    public float hitDuration = 0.2f;
+
+    private SpriteRenderer sr;
+    private Color originalColor;
+    private bool isHit = false;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        originalColor = sr.color;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("obstacle") && !isHit)
+        {
+            _ = FlashRedAsync();
+        }
+    }
+
+    private async UniTask FlashRedAsync()
+    {
+        isHit = true;
+        sr.color = hitColor;
+        await UniTask.Delay((int)(hitDuration * 1000));
+        sr.color = originalColor;
+        isHit = false;
+    }
+}
